17ff2147c9280c46e68dc72e50a2e0c33ea86f78
Adam <adam.jarrett.clare@gmail.com>
2021-03-24 14:55:21 -0600

    Simple script to read an image and display it. Additionaly convert an rgb image to grayscale

---
+++ basic_image_io.m
@@ -0,0 +1,27 @@
+%% Basic image input/output
+% Using matlab to read image, display image, and save image
+% Using KITTI data sets for my image source
+
+%%
+% Path to kitti data set
+im_path = '/Volumes/swap/projects/2011_09_29/2011_09_29_drive_0071_extract/';
+
+figure;
+hold on
+title('Camera 0')
+gray_img = imread([im_path, '/image_00/data/0000000000.png']);
+imshow(gray_img);
+
+rgb_img = imread([im_path, '/image_02/data/0000000000.png']);
+figure;
+subplot(2, 1, 1);
+hold on
+title('Camera 2')
+imshow(rgb_img);
+
+subplot(2, 1, 2);
+hold on
+title('Converted to grayscale');
+imshow(rgb2gray(rgb_img));
+
+print(gcf, 'camera2.png', '-dpng');
